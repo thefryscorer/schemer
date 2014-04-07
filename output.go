306c69ecd17452f5df8764604ffd805caf81fb69
duckness <duckn355@gmail.com>
2014-04-07 12:28:27 +0800

    add iTerm2 support

--- output.go
+++ output.go
@@ -109,3 +109,33 @@ func printRoxTerm(colors []color.Color) string {
 
 	return output
 }
+
+func printITerm2(colors []color.Color) string {
+	output := "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+	output += "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n"
+	output += "<plist version=\"1.0\">\n"
+	output += "<dict>\n"
+	for i, c := range colors {
+		cc := c.(color.NRGBA)
+		output += "\t<key>Ansi "
+		output += strconv.Itoa(i)
+		output += " Color</key>\n"
+		output += "\t<dict>\n"
+		output += "\t\t<key>Blue Component</key>\n"
+		output += "\t\t<real>"
+		output += strconv.FormatFloat(float64(cc.B) / 255, 'f', 17, 64)
+		output += "</real>\n"
+		output += "\t\t<key>Green Component</key>\n"
+		output += "\t\t<real>"
+		output += strconv.FormatFloat(float64(cc.G) / 255, 'f', 17, 64)
+		output += "</real>\n"
+		output += "\t\t<key>Red Component</key>\n"
+		output += "\t\t<real>"
+		output += strconv.FormatFloat(float64(cc.R) / 255, 'f', 17, 64)
+		output += "</real>\n"
+		output += "\t</dict>\n"
+	}
+	output += "</dict>\n"
+	output += "</plist>\n"
+	return output
+}
